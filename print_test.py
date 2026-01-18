777fa4bd4078d4c7c6b09b97e767c7652f04d25e
shen <she6510@floridapoly.edu>
2026-01-18 00:35:33 +0000

    COP2080HW1

---
+++ print_test.py
@@ -0,0 +1,6 @@
+my_name = "Shengcong He"
+print(my_name)
+print("Use 4 spaces per indentation level.")
+print("A Python program is divided into a number of logical lines.")
+print("The most significant outcome I hope to gain by taking this course is building the cornerstone of mastering Python in order to expand skills and knowledge into other advanced technologies. " \
+"Then, other outcomes that I wish to gain may be building my resume and being able to work on projects with Python involved.")
